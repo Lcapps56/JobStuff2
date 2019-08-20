f245fb43be9a9f67fa4ebf237f4a6172b314b59b
lcapps <lcapps5600@gmail.com>
2019-08-20 15:57:30 -0400

    new api Routes

--- public/js/index.js
+++ public/js/index.js
@@ -1,44 +1,44 @@
-$(document).ready(function(event){
-  $("path").on("click", function() {
+$(document).ready(function (event) {
+  $("path").on("click", function () {
     var mapState = $(this).attr("aria-label").trim();
     console.log(mapState)
   })
 
 
 
-$("#submit").on("click", function(event){
+  $("#submit").on("click", function (event) {
     var Values = {
-    type: $("#type").val(),
-    location: $("#location").val(),
-    job: $("#jobsList").val()
-  }
-  console.log(Values)
-  
-  // $.ajax("/results",{
-  // type: "POST",
-  // data: Values
-  // }).then(function(event){
-  //   console.log("sent")
-  // })
-})
-
-
-$(document).ready(function(){
-  var jobs = []
-
-  $.ajax({
-    URL: "/api/jobs",
-    type: "GET"
-  }).then(function(response){
-    for(var i=0; i<response.length; i++){
-      jobs.push(reponse[i].occ_title)
+      type: $("#type").val(),
+      location: $("#location").val(),
+      job: $("#jobsList").val()
     }
-    console.log(jobs)
-    $("#jobsList").select2({
-      data:jobs
-    })
+    console.log(Values)
+
+    // $.ajax("/results",{
+    // type: "POST",
+    // data: Values
+    // }).then(function(event){
+    //   console.log("sent")
+    // })
   })
 
-});
+
+  $(document).ready(function () {
+    var jobs = []
+
+    $.ajax({
+      URL: "/api/jobs",
+      type: "GET"
+    }).then(function (response) {
+      for (var i = 0; i < response.length; i++) {
+        jobs.push(reponse[i].occ_title)
+      }
+      console.log(jobs)
+      $("#jobsList").select2({
+        data: jobs
+      })
+    })
+
+  });
 
 })
