820a407cf249bd5c07efd38742d9cf9ef4b815ce
lcapps <lcapps5600@gmail.com>
2019-08-20 14:18:43 -0400

    homePage aprt 3 still not finished

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,7 @@ var path = require("path");
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/HTML/index.html"))
+    res.sendFile(path.join(__dirname, "../public/HTML/index.html", ))
   });
 
   // Load example page and pass in an example by id
@@ -14,7 +14,7 @@ module.exports = function (app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
+  // app.get("*", function (req, res) {
+  //   res.render("404");
+  // });
 };
