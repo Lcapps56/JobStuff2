a49250fd163fea7c3402599bf04fda4b3f7ad75d
lcapps <lcapps5600@gmail.com>
2019-08-21 11:38:31 -0400

    first push with finished dropdown

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -51,50 +51,50 @@ module.exports = function (app) {
       })
     };
 
-    // if (req.params.query === "4") {
-    //   // AVG salary for this job
+    if (req.params.query === "4") {
+      // AVG salary for this job
+      db.Job.findAll({
+        where: {
+          occ_title: req.params.Job
+        },
+        limit: 10,
+        order: [["a_pct50", "DESC"]]
+      }).then(function (data) {
+        res.json(data)
+        db.Job.findAll({
+          where: {
+            occ_title: req.params.Job
+          },
+          limit: 10,
+          order: [["h_pct50", "DESC"]]
+        });
+      });
+    };
+
+    // if (req.params.query === "5") {
+    //   // everything about a job
     //   db.Job.findAll({
     //     where: {
-    //       occ_title: req.params.Job
+    //       occ_code: req.params.code
     //     },
     //     limit: 10,
-    //     order: [["a_pct50", "DESC"]]
+    //     order: [["jobs_1000"], ["tot_emp"], ["h_mean"], ["a_mean"], ["h_pct10"], ["h_pct25"], ["h_pct50"], ["h_pct75"], ["h_pct90"], ["a_pct10"], ["a_pct25"], ["a_pct50"], ["a_pct75"], ["a_pct90"], ["annual"], ["hourly"]]
     //   }).then(function (data) {
-    //     res.json(data)
-    //     db.Job.findAll({
-    //       where: {
-    //         occ_title: req.params.Job
-    //       },
-    //       limit: 10,
-    //       order: [["h_pct50", "DESC"]]
-    //     });
-    //   });
-    // };
+    //     res.render("results", { results: data });
+    //   })
+    // }
 
-    if (req.params.query === "5") {
-      // everything about a job
-      db.Job.findAll({
-        where: {
-          occ_code: req.params.code
-        },
-        limit: 10,
-        order: [["jobs_1000"], ["tot_emp"], ["h_mean"], ["a_mean"], ["h_pct10"], ["h_pct25"], ["h_pct50"], ["h_pct75"], ["h_pct90"], ["a_pct10"], ["a_pct25"], ["a_pct50"], ["a_pct75"], ["a_pct90"], ["annual"], ["hourly"]]
-      }).then(function (data) {
-        res.render("results", { results: data });
-      })
-    }
-
-    if (req.params.query === "6") {
-      // everything about a state
-      db.Job.findAll({
-        where: {
-          area: req.params.state
-        },
-        limit: 10,
-        order: [["occ_title"], ["tot_emp"], ["h_mean"], ["a_mean"], ["h_pct10"], ["h_pct25"], ["h_pct50"], ["h_pct75"], ["h_pct90"], ["a_pct10"], ["a_pct25"], ["a_pct50"], ["a_pct75"], ["a_pct90"], ["annual"], ["hourly"]]
-      }).then(function (data) {
-        res.render("results", { results: data });
-      })
-    }
+    // if (req.params.query === "6") {
+    //   // everything about a state
+    //   db.Job.findAll({
+    //     where: {
+    //       area: req.params.state
+    //     },
+    //     limit: 10,
+    //     order: [["occ_title"], ["tot_emp"], ["h_mean"], ["a_mean"], ["h_pct10"], ["h_pct25"], ["h_pct50"], ["h_pct75"], ["h_pct90"], ["a_pct10"], ["a_pct25"], ["a_pct50"], ["a_pct75"], ["a_pct90"], ["annual"], ["hourly"]]
+    //   }).then(function (data) {
+    //     res.render("results", { results: data });
+    //   })
+    // }
   });
 };
